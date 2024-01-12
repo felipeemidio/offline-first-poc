19eebcbeed419866bc383d4902b6f15cdaea1629
felipeemidio <felipe.dev.emidio@gmail.com>
2024-01-12 10:52:34 -0300

    feat: add firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:offline_first_poc/datasources/firestore_datasource.dart';
+import 'package:offline_first_poc/firebase_options.dart';
 import 'package:offline_first_poc/main_app.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirestoreDatasource.initialize();
+
+  runApp(const MainApp());
 }
--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,11 +1,42 @@
 class Note {
-  final String id;
+  final String? id;
   final String content;
   final DateTime createdAt;
 
   const Note({
-    required this.id,
+    this.id,
     required this.content,
     required this.createdAt,
   });
+
+  bool get isSync => id != null;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'content': content,
+      'created_at': createdAt.toIso8601String(),
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map, String id) {
+    return Note(
+      id: id,
+      content: map['content'],
+      createdAt: DateTime.parse(map['created_at']),
+    );
+  }
+
+  @override
+  bool operator ==(covariant Note other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id && other.content == content && other.createdAt == createdAt;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ content.hashCode ^ createdAt.hashCode;
+
+  @override
+  String toString() => 'Note(id: $id, content: $content, createdAt: ${createdAt.toIso8601String()})';
 }
